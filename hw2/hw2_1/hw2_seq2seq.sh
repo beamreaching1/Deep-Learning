bdbd2dc3a610aa7dc052700a855263291151f4fd
beamreaching1 <wagnerctw@gmail.com>
2021-03-21 21:34:13 -0400

    Update hw2_seq2seq.sh

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -5,7 +5,7 @@ if ! [  -f ./modelBest-Current -a  -f ./DatasetPreload ]; then
 	rm ./DatasetPreload	
 	wget 'https://www.dropbox.com/s/a9r81uo1b2jqoo7/hw2.zip?dl=1' -O hw2.zip
 	unzip hw2.zip
-	#rm hw2.zip
+	rm hw2.zip
 fi
 
 python3 testModel.py $1 $2
